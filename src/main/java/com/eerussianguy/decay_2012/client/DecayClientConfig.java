d14c00984e3371eb091d030541bfe94c444391e1
Ant1och <180115878+Ant1och@users.noreply.github.com>
2025-06-23 11:25:55 +0300

    fix typo in the client config

--- src/main/java/com/eerussianguy/decay_2012/client/DecayClientConfig.java
+++ src/main/java/com/eerussianguy/decay_2012/client/DecayClientConfig.java
@@ -26,7 +26,7 @@ public class DecayClientConfig
         enableFoodShowsStackCount = builder.apply("enableFoodShowsStackCount").comment("If true, food stack counts will render behind the weight bar").define("enableFoodShowsStackCount", false);
         maxOunces = builder.apply("maxOunces").comment("The value that should be displayed as the maximum ounces (or whatever kind of weight type you choose) a food item could weigh. By default, a full stack is 160 ounces.").defineInRange("maxOunces", 16, 1, Integer.MAX_VALUE);
         enableFoodWeightRender = builder.apply("enableFoodWeightRender").comment("Enable rendering a food weight bar").define("enableFoodWeightRender", true);
-        enableFoodDecayRender = builder.apply("enableFoodDecayRender").comment("Enable rendering a food weight bar").define("enableFoodDecayRender", true);
+        enableFoodDecayRender = builder.apply("enableFoodDecayRender").comment("Enable rendering a food decay bar").define("enableFoodDecayRender", true);
         enableEuropeanMode = builder.apply("enableEuropeanMode").comment("If true, SI units will be used for food weight instead of the traditional oz. The name of this config option is a joke, I know SI units are used outside of Europe.").define("enableEuropeanMode", false);
 
         innerBuilder.pop();
